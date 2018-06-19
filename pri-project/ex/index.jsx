c5d036402fd9a3926a5610e207bfa0d919967d4c
Anderson Lima <anderson.rl.tr@gmail.com>
2018-06-19 13:59:20 -0300

    deletando arquivos desnecessarios

--- pri-project/ex/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Campo from './nav-component.jsx';
-
-
-ReactDOM.render(<Campo valor="Teste"></Campo>, document.getElementById('app'))
