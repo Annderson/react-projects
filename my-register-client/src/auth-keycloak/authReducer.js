0c524dfa034465b115ea4eb4c0956747ba992150
Anderson Lima <anderson.rl.tr@gmail.com>
2018-07-04 14:29:45 -0300

    alterações

--- my-register-client/src/auth-keycloak/authReducer.js
+++ my-register-client/src/auth-keycloak/authReducer.js
@@ -3,11 +3,11 @@ const INITIAL_STATE = {
     authenticated: false
 }
 
-export default (state = INITIAL_STATE, action) {
-
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'KEYCLOAK_CHANGE':
             return {...state, keycloak: action.payload }
+        default:
+            return state
     }
-
 }
