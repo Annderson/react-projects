3b0e04f0071cc2e5643d2f0022666840005d0b38
Anderson Lima <anderson.rl.tr@gmail.com>
2018-07-23 18:39:03 -0300

    teste

--- app-todo/frontend/src/main/multiselect-checkbox.jsx
+++ app-todo/frontend/src/main/multiselect-checkbox.jsx
@@ -1,16 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Multi from 'react-bootstrap-multiselect'
 import 'react-select/dist/react-select.css'
 
 
 const options = [
     { value: 'chocolate' },
-    { value: 'strawberry'},
-    { value: 'vanilla'}
-  ]
+    { value: 'strawberry' },
+    { value: 'vanilla' }
+]
 
 
-export default () => (
-    <Multi includeSelectAllOption
-          data={options} multiple="multiple"  />
+export default props => ( 
+    <Multi includeSelectAllOption data={options} multiple />
 )
